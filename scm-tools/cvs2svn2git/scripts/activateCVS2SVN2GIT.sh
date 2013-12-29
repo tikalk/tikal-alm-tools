a0e8883f9f95e67f11529aada14981e04daec9a0
Yoram Michaeli <yorammi@tikalk.com>
2013-12-29 17:47:41 +0200

    Add temp. work folder

--- scm-tools/cvs2svn2git/scripts/activateCVS2SVN2GIT.sh
+++ scm-tools/cvs2svn2git/scripts/activateCVS2SVN2GIT.sh
@@ -6,14 +6,16 @@
 # $3 - repository.name - repository name to migrate
 # $4 - authors.file.path - path to authors-file
 # $5 - git.remote.uri - URI of git repository for setting as remote origin and pushing the migrated CVS repository
+# $6 - temp.root.folder - temporary work folder root
 
 cvs2svn_tool_folder=$1
 cvs_repository_root=$2
 repository_name=$3
 authors_file_path=$4
 git_remote_uri=$5
+temp_root_folder=$6
 
-if [ "x$1" = "x" ] || [ "x$2" = "x" ] || [ "x$3" = "x" ] || [ "x$4" = "x" ] || [ "x$5" = "x" ]; then
+if [ "x$1" = "x" ] || [ "x$2" = "x" ] || [ "x$3" = "x" ] || [ "x$4" = "x" ] || [ "x$5" = "x" ] || [ "x$6" = "x" ]; then
   echo "usage:"
   echo "  activateCVS2SVN2GIT.sh <cvs2svn.tool.folder> <cvs.repository.root> <repository.name> <authors.file.path> <git.remote.uri>"
   echo "where:"
@@ -22,7 +24,8 @@ if [ "x$1" = "x" ] || [ "x$2" = "x" ] || [ "x$3" = "x" ] || [ "x$4" = "x" ] || [
 	echo " repository.name - repository name to migrate"
 	echo " authors.file.path - path to authors-file"
 	echo " git.remote.uri - URI of GIT repository for pushing the migrated CVS repository"
+	echo " temp.root.folder - temporary work folder root"
   exit -1
 fi
 
-ant -buildfile cvs2svn2git.xml -Dcvs2svn.tool.folder=$cvs2svn_tool_folder -Dcvs.repository.root=$cvs_repository_root -Drepository.name=$repository_name -Dauthors.file.path=$authors_file_path -Dgit.remote.uri=$git_remote_uri
+ant -buildfile cvs2svn2git.xml -Dcvs2svn.tool.folder=$cvs2svn_tool_folder -Dcvs.repository.root=$cvs_repository_root -Drepository.name=$repository_name -Dauthors.file.path=$authors_file_path -Dgit.remote.uri=$git_remote_uri -Dtemp.root.folder=$temp_root_folder
